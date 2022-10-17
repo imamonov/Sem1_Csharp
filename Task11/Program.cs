f02258ebb01e05419c9916a852fb3108694d50fd
ivan <iv.mamonow@yandex.ru>
2022-10-17 21:20:05 +0300

    Add Task 11 and Task 12

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая 
+// 1. выводит случайное трёхзначное число и 
+// 2. удаляет вторую цифру этого числа.
+// Например:
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = new Random().Next(99,1000);
+Console.WriteLine(num);
+
+int firstDigit = num / 100;
+int thirdDigit = num % 10;
+
+Console.WriteLine($"{firstDigit}{thirdDigit}");
---
+++ Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет 
+// 1. принимать на вход два числа и 
+// 2. выводить, является ли первое число кратным второму. 
+// 3. Если число 1 не кратно числу 2, то
+// 4. программа выводит остаток от деления.
+// Например:
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % numberB == 0)
+{
+    Console.WriteLine("Первое число является кратным второму");
+}
+else
+{
+    Console.WriteLine($"{numberA % numberB}");
+}
