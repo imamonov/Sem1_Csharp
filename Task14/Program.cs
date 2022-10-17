2a8f5c80d71886d290310caec1df31ee5f2993d2
ivan <iv.mamonow@yandex.ru>
2022-10-17 22:17:25 +0300

    Add Task 14 and Task 16

---
+++ Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool Number(int numA)
+{
+    int a = numA % 7;
+    int b = numA % 23;
+    return a == 0 && b == 0;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Number(number) ? "кратное" : "не кратно");
---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход два числа и 
+// 2. проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+bool CheckOnSquare(int digit1, int digit2)
+{
+    return digit1 * digit1 == digit2 || digit2 * digit2 == digit1;
+}
+
+Console.WriteLine("Введите число #1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число #2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+bool square = CheckOnSquare(numberA, numberB);
+Console.WriteLine(square ? "является квадратом" : "не является квадратом");
