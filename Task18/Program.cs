c2db6f34a64573d2ecb89a47012a84fe9f0efbb2
ivan <iv.mamonow@yandex.ru>
2022-10-27 01:36:48 +0300

    Add Task 18 and Task 20

---
+++ Task18/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая 
+// 1. по заданному номеру четверти, 
+// 2. показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+Console.WriteLine("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int quarters)
+{
+if(quarters == 1) return "X > 0 и Y > 0";
+if(quarters == 2) return "X < 0 и Y > 0";
+if(quarters == 3) return "X < 0 и Y < 0";
+if(quarters == 4) return "X > 0 и Y < 0";
+return "Заданной четверти не существует";
+}
+
+string result = Quarter(quarter);
+
+Console.WriteLine(result);
+
+
+// Решение через switch case:
+
+// string Range(int quarter)
+// {
+// switch (quarter)
+// {
+// case 1:
+// return "x > 0 и y > 0";
+// case 2:
+// return "x < 0 и y > 0";
+// case 3:
+// return "x < 0 и y < 0";
+// case 4:
+// return "x > 0 и y < 0";
+// default:
+// return "Некорректные данные";
+// }
+// }
+// Console.WriteLine("Введите номер четверти");
+// int quarter = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Range(quarter));
---
+++ Task20/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая
+// 1. принимает на вход координаты двух точек и
+// 2. находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+// double d = Math.Sqrt(45);
+
+// double s = 5.099965343;
+// double res = Math.Round(s, 2, MidpointRounding.ToZero);
+// System.Console.WriteLine(res);
+
+System.Console.WriteLine("Введите координаты точки A:");
+Console.Write("X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координаты точки B:");
+Console.Write("X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Distance(x1, y1, x2, y2);
+System.Console.WriteLine(Math.Round(distance, 2, MidpointRounding.ToZero));
+
+double Distance(int xc1, int yc1, int xc2, int yc2)
+{
+    double square = Math.Sqrt((xc1 - xc2) * (xc1 - xc2) + (yc1 - yc2) * (yc1 - yc2));
+    return square;
+}
